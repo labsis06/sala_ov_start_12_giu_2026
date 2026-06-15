2ba988942d77ee696af68dfc9f9e8ac91a598d46
labsis06 <102295090+labsis06@users.noreply.github.com>
2026-06-15 17:08:13 +0200

    spostamento tasto richiedi prenotazione

--- site/tmpl/calendar/default.php
+++ site/tmpl/calendar/default.php
@@ -11,9 +11,10 @@ require_once __DIR__ . '/../helpers/calendar.php';
   <header class="salaov-page-header">
     
     <p>Consulta i giorni disponibili per la visita alla Sala di Monitoraggio. Le richieste inviate restano in attesa di approvazione.</p>
+  <p class="salaov-main-cta"><a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_salaov&view=booking'); ?>">Richiedi prenotazione</a></p>
   </header>
 
   <?php echo salaovRenderAvailabilityCalendar($this->slots ?? [], $this->availability ?? [], ['months' => 6, 'dayRules' => $this->dayRules ?? [], 'daySlots' => $this->daySlots ?? []]); ?>
 
-  <p class="salaov-main-cta"><a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_salaov&view=booking'); ?>">Richiedi prenotazione</a></p>
+  
 </div>

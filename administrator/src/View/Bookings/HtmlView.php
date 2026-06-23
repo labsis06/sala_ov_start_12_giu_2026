295283383b410c24d8e5b7706b95e50a2bab4c99
labsis06 <102295090+labsis06@users.noreply.github.com>
2026-06-23 14:06:32 +0200

    edit booking

--- administrator/src/View/Bookings/HtmlView.php
+++ administrator/src/View/Bookings/HtmlView.php
@@ -1,5 +1,23 @@
 <?php
+
 namespace Ov\Component\Salaov\Administrator\View\Bookings;
+
 \defined('_JEXEC') or die;
+
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
-class HtmlView extends BaseHtmlView { public function display($tpl=null){ $model=$this->getModel(); $this->items=$model->getItems(); $this->staff=method_exists($model,'getStaff')?$model->getStaff():[]; parent::display($tpl); } }
+
+class HtmlView extends BaseHtmlView
+{
+    public function display($tpl = null)
+    {
+        $model = $this->getModel();
+
+        $this->items       = $model->getItems();
+        $this->staff       = method_exists($model, 'getStaff') ? $model->getStaff() : [];
+        $this->slots       = method_exists($model, 'getSlots') ? $model->getSlots() : [];
+        $this->languages   = method_exists($model, 'getLanguages') ? $model->getLanguages() : [];
+        $this->visitLevels = method_exists($model, 'getVisitLevels') ? $model->getVisitLevels() : [];
+
+        parent::display($tpl);
+    }
+}
